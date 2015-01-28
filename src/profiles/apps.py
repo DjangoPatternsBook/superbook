d5fa704729beb0424fa508731f766c213fa4519c
Arun Ravindran <arunvr@gmail.com>
2015-01-28 16:06:36 +0530

    Renamed 'accounts' app to 'profiles'

--- src/profiles/apps.py
+++ src/profiles/apps.py
@@ -2,7 +2,7 @@ from django.apps import AppConfig
 
 
 class ProfileConfig(AppConfig):
-    name = "accounts"
+    name = "profiles"
     verbose_name = 'User Profiles'
 
     def ready(self):
--- src/superbook/settings.py
+++ src/superbook/settings.py
@@ -57,7 +57,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'accounts',
+    'profiles',
 )
 
 MIDDLEWARE_CLASSES = (
