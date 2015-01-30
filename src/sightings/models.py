aebd0c830f8be884b008d203c888e7d0ba54da89
Arun Ravindran <arunvr@gmail.com>
2015-01-30 23:56:31 +0530

    Added str definitions for models in sighting app

--- src/sightings/models.py
+++ src/sightings/models.py
@@ -6,11 +6,18 @@ class Origin(models.Model):
     superhero = models.ForeignKey(settings.AUTH_USER_MODEL)
     origin = models.CharField(max_length=100)
 
+    def __str__(self):
+        return "{}'s orgin: {}".format(self.superhero, self.origin)
+
 
 class Location(models.Model):
     latitude = models.FloatField()
     longitude = models.FloatField()
     country = models.CharField(max_length=100)
+
+    def __str__(self):
+        return "{}: ({}, {})".format(self.country,
+                                     self.latitude, self.longitude)
 
     class Meta:
         unique_together = ("latitude", "longitude")
@@ -21,6 +28,13 @@ class Sighting(models.Model):
     power = models.CharField(max_length=100)
     location = models.ForeignKey(Location)
     sighted_on = models.DateTimeField()
+
+    def __str__(self):
+        return "{}'s power {} sighted at: {} on {}".format(
+            self.superhero,
+            self.power,
+            self.location.country,
+            self.sighted_on)
 
     class Meta:
         unique_together = ("superhero", "power")
