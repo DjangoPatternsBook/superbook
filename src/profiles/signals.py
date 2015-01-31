960ecda552b3a03a7b87e0f0806fc1e1d5c9b930
Arun Ravindran <arunvr@gmail.com>
2015-01-31 09:50:59 +0530

    Reordered imports based on Krace's review comment

--- src/profiles/signals.py
+++ src/profiles/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from . import models
 from django.conf import settings
+from . import models
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
