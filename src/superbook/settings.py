3f36ac2e6366fa9a2a807fdb009cc5669646f6be
Jim Munro <jimfmunro@gmail.com>
2015-04-02 21:48:54 -0600

    removed bootstrap3 setting removed in this commit 4f61ed348c773c9ff06d6d63fae4ab8bcefe9060

--- src/superbook/settings.py
+++ src/superbook/settings.py
@@ -46,7 +46,6 @@ if "celery" in sys.argv[0]:
 # Application definition
 
 INSTALLED_APPS = (
-    'django_admin_bootstrapped.bootstrap3',
     'django_admin_bootstrapped',
     'crispy_forms',
     'chapter05',
