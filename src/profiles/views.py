054b695a6daab0880dcb3046e952035d81c61ef6
Arun Ravindran <arunvr@gmail.com>
2015-11-08 23:47:34 +0530

    Django 1.8 warnings fixed

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -69,6 +69,7 @@ class SignInAndSignUp(generic.edit.FormMixin, generic.TemplateView):
 
 class LogoutView(generic.RedirectView):
     url = reverse_lazy("home")
+    permanent = True
 
     def get(self, request, *args, **kwargs):
         logout(request)
